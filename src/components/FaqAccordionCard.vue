2f9158694eb535646e8f76b87255ba9214446f0f
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2022-10-25 23:04:59 -0300

    style: mobile styles finished

--- src/components/FaqAccordionCard.vue
+++ src/components/FaqAccordionCard.vue
@@ -69,22 +69,25 @@ const faqs = ref([
     <div class="faq-card__content-container">
       <!-- # faq__heading -->
       <h1 class="faq-card__heading">FAQ</h1>
-      <!-- # faq__questions-container -->
-      <details
-        class="faq-card__questions-container"
-        v-for="faq in faqs"
-        :key="faq.id"
-      >
-        <summary class="faq-card__question active">
-          {{ faq.question }}
-          <img
-            class="faq-card__icon"
-            :src="iconArrowDown"
-            alt="click to reveal answer"
-          />
-        </summary>
-        <p class="faq-card__answer">{{ faq.answer }}</p>
-      </details>
+
+      <div class="faq-card__qa-container">
+        <!-- # faq__questions-container -->
+        <details
+          class="faq-card__questions-container"
+          v-for="faq in faqs"
+          :key="faq.id"
+        >
+          <summary class="faq-card__question active">
+            {{ faq.question }}
+            <img
+              class="faq-card__icon"
+              :src="iconArrowDown"
+              alt="click to reveal answer"
+            />
+          </summary>
+          <p class="faq-card__answer">{{ faq.answer }}</p>
+        </details>
+      </div>
     </div>
   </article>
 </template>
@@ -126,15 +129,19 @@ const faqs = ref([
   text-align: center;
   font-weight: 700;
   color: var(--pri-blue-very-dark-desaturated);
+  margin-bottom: 1.9rem;
 }
 
-/* details {
-  border-bottom: 1px solid var(--dividers-blue-light-grayish);
-  transition: all 2s ease;
-} */
 .faq-card__questions-container {
   border-bottom: 1px solid var(--dividers-blue-light-grayish);
-  transition: all 2s ease;
+  padding: 1.2rem 0;
+}
+
+.faq-card__qa-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 29.5rem;
 }
 .faq-card__question {
   display: flex;
@@ -143,13 +150,22 @@ const faqs = ref([
   font-size: 1.3rem;
   line-height: 1.6;
   color: var(--neut-blue-very-dark-grayish);
+  letter-spacing: 0.01rem;
+  cursor: pointer;
+  transition: margin 0.5s ease;
+}
+
+.faq-card__question:hover {
+  color: var(--pri-red-soft);
 }
 
 details[open] > .faq-card__question {
   font-size: 1.3rem;
   font-weight: 700;
+  margin-bottom: 0.5rem;
   color: var(--pri-blue-very-dark-desaturated);
 }
+
 .faq-card__icon {
   width: 0.8rem;
   transition: all 0.3s;
@@ -158,10 +174,47 @@ details[open] > .faq-card__question {
 details[open] .faq-card__icon {
   transform: rotate(180deg);
 }
+
 .faq-card__answer {
   color: var(--neut-blue-dark-grayish);
+  width: 95%;
+  opacity: 0;
   line-height: 1.8;
-  letter-spacing: 0.02rem;
-  transition: all 3s;
+  letter-spacing: 0.004rem;
+  transition: all 3s ease;
+}
+
+details[open] .faq-card__answer {
+  opacity: 1;
+}
+
+/* 1440 = 90em */
+@media (min-width: 90em) {
+  .faq-card {
+    width: 92rem;
+    height: 50.9rem;
+    padding: 6.5rem 9.5rem 8.3rem 47.5rem;
+  }
+
+  .faq-card__illus-container {
+  }
+
+  .faq-card__heading {
+    text-align: left;
+  }
+
+  .faq-card__questions-container {
+  }
+
+  .faq-card__question {
+    font-size: 1.4rem;
+  }
+
+  details[open] > .faq-card__question {
+    font-size: 1.4rem;
+  }
+
+  .faq-card__answer {
+  }
 }
 </style>
