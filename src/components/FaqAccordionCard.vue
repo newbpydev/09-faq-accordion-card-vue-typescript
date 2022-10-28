b39a0788ccd5059f05bce67c82b1dca209d8b8c4
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2022-10-28 10:19:21 -0300

    style: finish desktop layout and styles

--- src/components/FaqAccordionCard.vue
+++ src/components/FaqAccordionCard.vue
@@ -49,44 +49,34 @@ const faqs = ref([
 <!-- * TEMPLATE -->
 <template>
   <!-- @ faq -->
-  <article class="faq-card">
-    <!-- # faq__illus-container -->
-    <div class="faq-card__illus-container">
-      <img
-        class="faq-card__illus"
-        :src="illusWomanMobile"
-        alt=""
-        aria-hidden="true"
-      />
-      <img
-        class="faq-card__illus-shadow"
-        :src="bgPatternMobile"
-        alt=""
-        aria-hidden="true"
-      />
-    </div>
-
-    <div class="faq-card__content-container">
-      <!-- # faq__heading -->
-      <h1 class="faq-card__heading">FAQ</h1>
-
-      <div class="faq-card__qa-container">
-        <!-- # faq__questions-container -->
-        <details
-          class="faq-card__questions-container"
-          v-for="faq in faqs"
-          :key="faq.id"
-        >
-          <summary class="faq-card__question active">
-            {{ faq.question }}
-            <img
-              class="faq-card__icon"
-              :src="iconArrowDown"
-              alt="click to reveal answer"
-            />
-          </summary>
-          <p class="faq-card__answer">{{ faq.answer }}</p>
-        </details>
+  <article class="faq">
+    <div class="faq-card">
+      <!-- # faq__illus-container -->
+      <div class="faq-card__illus-container">
+        <div class="faq-card__illus" aria-hidden="true" role="img" />
+        <div class="faq-card__illus-shadow" role="img" aria-hidden="true" />
+      </div>
+      <div class="faq-card__content-container">
+        <!-- # faq__heading -->
+        <h1 class="faq-card__heading">FAQ</h1>
+        <div class="faq-card__qa-container">
+          <!-- # faq__questions-container -->
+          <details
+            class="faq-card__questions-container"
+            v-for="faq in faqs"
+            :key="faq.id"
+          >
+            <summary class="faq-card__question active">
+              {{ faq.question }}
+              <img
+                class="faq-card__icon"
+                :src="iconArrowDown"
+                alt="click to reveal answer"
+              />
+            </summary>
+            <p class="faq-card__answer">{{ faq.answer }}</p>
+          </details>
+        </div>
       </div>
     </div>
   </article>
@@ -94,6 +84,9 @@ const faqs = ref([
 
 <!-- * STYLE -->
 <style scoped>
+.faq {
+  margin-top: 6.3rem;
+}
 .faq-card {
   position: relative;
   width: 32.7rem;
@@ -105,23 +98,42 @@ const faqs = ref([
   /* TODO: add top margin to match design */
   box-shadow: 0 5rem 5rem -2rem rgba(53 18 122 / 0.497159);
 }
-
-.faq-card__illus {
-  z-index: 5;
-}
-.faq-card__illus-shadow {
-  z-index: -1;
-  position: absolute;
-  bottom: -2.8rem;
-  left: 0;
-}
 .faq-card__illus-container {
   position: absolute;
-  top: -10.6rem;
+  top: -10.8rem;
   left: 50%;
   transform: translate(-50%);
   width: 23.7rem;
   height: auto;
+}
+
+.faq-card__illus {
+  z-index: 5;
+  height: 18rem;
+  width: auto;
+  background-image: url("@/assets/images/illustration-woman-online-mobile.svg");
+}
+.faq-card__illus::after {
+  z-index: -1;
+  content: "";
+  position: absolute;
+  top: 3rem;
+  height: 0rem;
+  background: transparent;
+  width: 100%;
+  box-shadow: 0 10rem 7rem 1rem rgba(53 18 122 / 0.497159);
+}
+.faq-card__illus-shadow {
+  z-index: -1;
+  position: absolute;
+  top: 10.8rem;
+  left: 0rem;
+
+  width: 24rem;
+  height: 14.5rem;
+
+  background-image: url("@/assets/images/bg-pattern-mobile.svg");
+  background-repeat: no-repeat;
 }
 
 .faq-card__heading {
@@ -190,14 +202,61 @@ details[open] .faq-card__answer {
 
 /* 1440 = 90em */
 @media (min-width: 90em) {
+  .faq {
+    position: relative;
+    margin-top: 0;
+  }
+
   .faq-card {
     width: 92rem;
     height: 50.9rem;
     padding: 6.5rem 9.5rem 8.3rem 47.5rem;
     margin-left: 4.5rem;
+    overflow: hidden;
+  }
+
+  .faq::before {
+    content: "";
+    position: absolute;
+    top: 20.3rem;
+    left: -4.8rem;
+    height: 15rem;
+    width: 19.1rem;
+    z-index: 99;
+
+    background-image: url("@/assets/images/illustration-box-desktop.svg");
+  }
+  .faq::after {
+    content: "";
+    position: absolute;
+    top: 18rem;
+    left: 0rem;
+    width: 9rem;
+    height: 10rem;
+    box-shadow: 0 5rem 6rem -2rem rgba(0 0 0 / 1);
   }
 
   .faq-card__illus-container {
+    top: 48.7%;
+    left: -6.7%;
+    transform: translate(-10%, -50%);
+  }
+
+  .faq-card__illus {
+    position: relative;
+    height: 35.9rem;
+    width: 47.2rem;
+    background-image: url("@/assets/images/illustration-woman-online-desktop.svg");
+  }
+
+  .faq-card__illus-shadow {
+    position: absolute;
+    top: -36.5rem;
+    left: -49.2rem;
+    height: 94.4rem;
+    width: 97rem;
+    background-image: url("@/assets/images/bg-pattern-desktop.svg");
+    background-repeat: no-repeat;
   }
 
   .faq-card__heading {
@@ -209,6 +268,7 @@ details[open] .faq-card__answer {
 
   .faq-card__question {
     font-size: 1.4rem;
+    padding-right: 1.7rem;
   }
 
   details[open] > .faq-card__question {
